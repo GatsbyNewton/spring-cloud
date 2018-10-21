5fa975089ff424bb4170cade31854a4b5e89f728
GatsbyNewton <wangzmking@163.com>
2018-10-21 15:36:43 +0800

    Config Server

---
+++ config-server/src/main/java/edu/wzm/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package edu.wzm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Hello world!
+ *
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+    public static void main( String[] args ){
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
